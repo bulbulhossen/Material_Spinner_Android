ddce9ea5a9d033ed42049f8de3d2dc7caf90f6a3
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-30 11:59:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/materialspinnerandroid/Another_activity.java
@@ -0,0 +1,18 @@
+package com.example.appsd.materialspinnerandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appsd on 1/30/2016.
+ */
+public class Another_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.another);
+
+
+    }
+}
